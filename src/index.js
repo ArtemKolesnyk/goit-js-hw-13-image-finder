206237edac3378dc6f366f5c3ac0a769c60761f5
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-11-11 15:16:51 +0100

    add styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 import './sass/main.scss';
+
+import '@pnotify/core/dist/BrightTheme.css';
+const { error } = require('@pnotify/core');
+
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+import imgTemp from './templates/cardImg.hbs';
+import getRefs from './js/getRefs';
+const refs = getRefs();
+
+import PixabayApiService from './js/apiService';
+const pixabayApiService = new PixabayApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+refs.galleryList.addEventListener('click', openLargeImage);
+refs.loadMore.style.display = 'none';
+
+function onSearch(e) {
+  e.preventDefault();
+  refs.galleryList.innerHTML = '';
+
+  pixabayApiService.query = e.currentTarget.elements.query.value;
+  if (pixabayApiService.query === '') {
+    return error({
+      text: 'Введите текст для поиска!',
+      delay: 300,
+    });
+  }
+
+  clearHitsContainer();
+  pixabayApiService.resetPage();
+  pixabayApiService.fetchPhotos().then(appendImgMarkup);
+}
+function onLoadMore() {
+  pixabayApiService.fetchPhotos().then(appendImgMarkup);
+}
+
+function appendImgMarkup(image) {
+  refs.loadMore.style.display = 'block';
+  refs.galleryList.insertAdjacentHTML('beforeend', imgTemp(image));
+  scrollInto();
+}
+function clearHitsContainer() {
+  refs.galleryList.innerHTML = '';
+}
+function scrollInto() {
+  refs.loadMore.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+
+function openLargeImage(e) {
+  if (!e.target.dataset.source) {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" />`);
+  instance.show();
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,36 +2,32 @@ const BASE_URL = 'https://pixabay.com/api';
 const API_URL = '24263315-36ea98cb6d0ce52f0b1c6e073';
 
 export default class PixabayApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPhotos() {
+    return fetch(
+      `${BASE_URL}/?key=${API_URL}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`,
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
 
- 
-    fetchImages() {
-        return fetch(
-            `${BASE_URL}/?key=${API_URL}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`,
-        )
-        .then(response => response.json())
-        .then(({hits}) => {
-            this.enlargePage();
-            return hits;
-        })   
-    }
+        return hits;
+      });
+  }
 
-    enlargePage() {
-        this.page +=1;
-    }
-
-    resetPage() {
-        this.page = 1;
-    }
-    
-    get query() {
-        return this.searchQuery;
-    }
-    
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
